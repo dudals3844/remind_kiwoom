feab213ca1c25c2f4c191698d4ff1f630ada38a7
YoungMin <dudals3844@gmail.com>
2021-01-09 21:10:30 +0900

    Add request deposit info

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -10,6 +10,15 @@ class Kiwoom(QAxWidget):
 
         self.login_event_loop = QEventLoop()
 
+        #### 계좌 관련 변수
+        self.account_num = None
+        self.deposit = 0
+        self.use_money = 0
+        self.use_money_percent = 0.5
+        self.output_deposit = 0
+
+        self.screen_info = "2000"
+
         self.event_slots()
         self.signal_login_commConnect()
         self.get_account_info()
@@ -34,3 +43,9 @@ class Kiwoom(QAxWidget):
         account_num = account_list.split(',')[0]
         self.account_num = account_num
         print(f'계좌번호: {self.account_num}')
+    def detail_account_info(self, sPreNext = '0'):
+        self.dynamicCall("SetInputValue(QString, QString)","계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(QString, QString)","비밀번호", "0000")
+        self.dynamicCall("SetInputValue(QString, QString)","비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(QString, QString)","조회구분", "1")
+        self.dynamicCall("CommRqData(QString, QString, int, QString)","예수금상세현황요청", "opw00001", sPreNext, self.screen_info)
