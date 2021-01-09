56d6b9ca17134caa4cc9138f700e7dc9acb6a626
YoungMin <dudals3844@gmail.com>
2021-01-09 23:58:14 +0900

    Add detail account mystock

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -23,6 +23,7 @@ class Kiwoom(QAxWidget):
         self.signal_login_commConnect()
         self.get_account_info()
         self.detail_account_info()
+        self.detail_account_mystock()
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
@@ -40,15 +41,29 @@ class Kiwoom(QAxWidget):
         self.login_event_loop.exit()
 
     def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
-        deposit = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, '예수금')
-        self.deposit = int(deposit)
+        if sRQName == '예수금상세현황요청:':
+            deposit = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, '예수금')
+            self.deposit = int(deposit)
 
-        use_money = float(self.deposit) * self.use_money_percent
-        self.use_money = int(use_money) / 4
-        output_deposit = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, '출금가능금액')
-        self.output_deposit = int(output_deposit)
-        print(f'예수금: {self.output_deposit}')
-        self.stop_screen_cancle(self.screen_my_info)
+            use_money = float(self.deposit) * self.use_money_percent
+            self.use_money = int(use_money) / 4
+            output_deposit = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, '출금가능금액')
+            self.output_deposit = int(output_deposit)
+            print(f'예수금: {self.output_deposit}')
+            self.stop_screen_cancle(self.screen_my_info)
+
+        elif sRQName == '계좌평가내역요청':
+            total_buy_money = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0,
+                                               "총매입금액")
+            self.total_buy_money = int(total_buy_money)
+            total_profit_loss_money = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName,
+                                                       0, "총평가손익금액")
+            self.total_profit_loss_money = int(total_profit_loss_money)
+            total_profit_loss_rate = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName,
+                                                      0, "총수익률(%)")
+            self.total_profit_loss_rate = float(total_profit_loss_rate)
+            print(f'계좌평가잔고내역요청: {self.total_buy_money}, {self.total_profit_loss_money}, {self.total_profit_loss_rate}')
+
 
     def get_account_info(self):
         account_list = self.dynamicCall('GetLoginInfo(QString)', 'ACCNO')
@@ -56,12 +71,20 @@ class Kiwoom(QAxWidget):
         self.account_num = account_num
         print(f'계좌번호: {self.account_num}')
 
-    def detail_account_info(self, sPreNext = '0'):
+    def detail_account_info(self, sPrevNext = '0'):
         self.dynamicCall("SetInputValue(QString, QString)","계좌번호", self.account_num)
         self.dynamicCall("SetInputValue(QString, QString)","비밀번호", "0000")
         self.dynamicCall("SetInputValue(QString, QString)","비밀번호입력매체구분", "00")
         self.dynamicCall("SetInputValue(QString, QString)","조회구분", "1")
-        self.dynamicCall("CommRqData(QString, QString, int, QString)","예수금상세현황요청", "opw00001", sPreNext, self.screen_my_info)
+        self.dynamicCall("CommRqData(QString, QString, int, QString)","예수금상세현황요청", "opw00001", sPrevNext, self.screen_my_info)
+
+    def detail_account_mystock(self, sPrevNext="0"):
+        self.dynamicCall("SetInputValue(QString, QString)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(QString, QString)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(QString, QString)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(QString, QString)", "조회구분", "1")
+        self.dynamicCall("CommRqData(QString, QString, int, QString)", "계좌평가잔고내역요", "opw00018", sPrevNext,
+                         self.screen_my_info)
 
     def stop_screen_cancle(self, sScrNo = None):
         self.dynamicCall('DisconnectRealData(QString)', sScrNo)
