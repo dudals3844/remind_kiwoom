5c721831e95304bd96a8c9d77948453730c305b7
choiyoungmin <dudals3844@gmail.com>
2021-02-12 16:11:01 +0900

    Add screen number enum class

---
+++ property/screen_number.py
@@ -0,0 +1,11 @@
+import enum
+
+
+class ScreenNumber(enum.Enum):
+    MY_INFO = "2000"
+    CALCULATION_STOCK = "4000"
+    START_STOP_REAL = "1000"
+    REAL_STOCK_DATE = "1500"
+    ORDER = "3000"
+
+
