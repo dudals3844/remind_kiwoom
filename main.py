ab916f107a6c2e8d84ffdaf70b7139f040e1529f
choiyoungmin <dudals3844@gmail.com>
2021-02-13 00:14:30 +0900

    Modify Kiwoom object to Trading objects

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from kiwoom.kiwoom import Kiwoom
 import sys
 from PyQt5.QtWidgets import *
 
+from kiwoom.trading import Trading
+
+
 class Main:
     def __init__(self):
         print('Main start')
 
         self.app = QApplication(sys.argv)
-        self.kiwoom = Kiwoom()
+        self.kiwoom = Trading()
         self.app.exec_()
 
 if __name__ == '__main__':
