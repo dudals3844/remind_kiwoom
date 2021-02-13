d9d1d83189bf57cf22c44471b2a6a0ecf9e8c2f7
choiyoungmin <dudals3844@gmail.com>
2021-02-13 11:31:48 +0900

    Modify set account data

--- kiwoom/tr/account.py
+++ kiwoom/tr/account.py
@@ -1,8 +1,8 @@
-
+from property.global_variable import ACCOUNT_DATA
 
 class Account:
     def get_info(self):
         account_list = self.dynamicCall('GetLoginInfo(QString)', 'ACCNO')
         account_num = account_list.split(';')[0]
         print(f'계좌번호: {account_num}')
-        return account_num
+        ACCOUNT_DATA.number = account_num
