ee87000188e6b0d903eac4b70ba4320f6a330953
choiyoungmin <dudals3844@gmail.com>
2021-02-14 15:54:17 +0900

    Add request detail deposit and receive deposit

--- kiwoom/trading.py
+++ kiwoom/trading.py
@@ -7,6 +7,7 @@ from kiwoom.tr.account import Account
 from property.global_variable import TR_EVENTLOOP, ACCOUNT_DATA
 from kiwoom.tr.login import Login
 from kiwoom.slot.login_slot import LoginSlot
+from kiwoom.slot.tr_slot import TrSlot
 
 
 class Trading(QAxWidget):
@@ -15,10 +16,12 @@ class Trading(QAxWidget):
         print("Kiwoom start")
         self.get_ocx_instance()
         LoginSlot.connect(self, self.login_slot)
+        TrSlot.connect(self, self.trdata_slot)
 
         Login.request(self)
-        Account.get_info(self)
-        print(ACCOUNT_DATA.number)
+        Account.receive_info(self)
+        Account.request_detail_deposit(self)
+
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
@@ -26,3 +29,7 @@ class Trading(QAxWidget):
     def login_slot(self, err_code):
         print(errors(err_code)[1])
         TR_EVENTLOOP.exit()
+
+    def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
+        if sRQName == '예수금상세현황요청':
+            Account.receive_detail_deposit(self, sTrCode, sRQName)
