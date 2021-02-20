d5e9d307f9119a3c972749c1a5c244dd7a2268be
choiyoungmin <dudals3844@gmail.com>
2021-02-21 00:01:01 +0900

    Change dataclass variable

--- property/open_order_stock_dataclass.py
+++ property/open_order_stock_dataclass.py
@@ -3,11 +3,21 @@ from dataclasses import dataclass
 
 @dataclass
 class StockOpenOrderData:
+    # 기업명
     name: str = None
+    # 종목코드
     code: str = None
+    # 주문상태 (접수, 체결, 확인)
     status: str = None
-    total_order_quantity: int = None
-    quantity: int = None
+    # 주문수량
+    order_quantity: int = None
+    # 미체결 수량
+    unfill_quantity: int = None
+    # 주문가격
     price: float = None
-    number: str = None
+    # 구분 (매수, 매도, 매수정정, 매도정정)
+    gubun: str = None
+    # 주문번호
+    number: int = None
+    # 원주문번호
     origin_number: str = None
