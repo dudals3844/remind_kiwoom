021c9d5d49e83309d603c7522492f3492bdda075
choiyoungmin <dudals3844@gmail.com>
2021-02-25 16:15:19 +0900

    Add Real Type object

--- property/global_variable.py
+++ property/global_variable.py
@@ -1,7 +1,9 @@
 from PyQt5.QtCore import QEventLoop
-from property.account_dataclass import AccountData
+from config.kiwoomType import RealType
+from property.dataclass.account_dataclass import AccountData
 
 TR_EVENTLOOP = QEventLoop()
 ACCOUNT_DATA = AccountData()
 HOLD_STOCK_LIST = []
 OPEN_ORDER_STOCK_LIST = []
+REAL_TYPE = RealType()
